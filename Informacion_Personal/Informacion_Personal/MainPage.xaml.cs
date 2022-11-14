c9ae1d675c4a6442f7f4af7f4e29d49410b153ca
alfre <alfre@DESKTOP-S1GAVLV>
2022-11-14 10:37:21 -0600

    Agregar archivos de proyecto.

---
+++ Informacion_Personal/Informacion_Personal/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Informacion_Personal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviarInformacion_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Informacion
+            {
+                Nombre = txtnombre.Text,
+                Apellido = txtapellido.Text,
+                Edad = Convert.ToInt32(txtedad.Text),
+                Correo = txtcorreo.Text
+            };
+            var page = new Views.InfoPersonal();
+            page.BindingContext = emple;
+            await Navigation.PushModalAsync(page);
+        }
+    }
+}
---
+++ Informacion_Personal/Informacion_Personal/Models/Informacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Informacion_Personal.Models
+{
+    class Informacion
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+    }
+}
